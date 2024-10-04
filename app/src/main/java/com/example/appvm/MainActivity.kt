a126143a316a157890edc9ef0c4da4776facc34d
Andyx34 <andreasolarte56@gmail.com>
2024-10-04 11:29:06 -0500

    n commit

--- app/src/main/java/com/example/appvm/MainActivity.kt
+++ app/src/main/java/com/example/appvm/MainActivity.kt
@@ -6,22 +6,36 @@ import android.view.View
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.appvm.screens.CreditCardScreen
 import com.example.appvm.ui.theme.AppVMTheme
+import com.example.appvm.viewModel.CreditCardViewModel
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
+    private val viewModel:CreditCardViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AppVMTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    CreditCardScreen(viewModel)
+                }
+            }
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -31,7 +45,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier, viewModel: GreetingViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
